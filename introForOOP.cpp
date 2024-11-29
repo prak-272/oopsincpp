417819f9a9ba47cbcb45a802395d4eb72f2ce40a
Prakhar Kumar <prakharkumar272@gmail.com>
2024-11-30 04:00:43 +0530

    constructors and member funcs

--- introForOOP.cpp
+++ introForOOP.cpp
@@ -8,25 +8,35 @@ public:
     string OwnerName;
     int subscriberCount;
     list<string> publishedVideoTitle;
-    
+
+    youTubeChannel(string name,string ownerName){
+        Name = name;
+        OwnerName = ownerName;
+        subscriberCount = 0;
+    }
+
+    void printInformation(){
+        cout << "Name" << " " << Name << endl;
+        cout << "OwnerName" << " " << OwnerName << endl;
+        cout << "Subscriber Count" << " " <<  subscriberCount << endl;
+        cout << "Published Video Title" << endl;
+        for(string videoTitle:publishedVideoTitle) {
+          cout << videoTitle << endl;   
+        }
+
+    }
+
 };
 
 int main() {
     
-    youTubeChannel ytChannel;
+    youTubeChannel ytChannel("coder","prakhar kumar");
+    youTubeChannel ytChannel2("prak","prakhar");
 
-    ytChannel.Name = "Coder";
-    ytChannel.OwnerName = "Prakhar";
-    ytChannel.subscriberCount = 4000;
-    ytChannel.publishedVideoTitle = {"video1","video2"};
+    ytChannel.publishedVideoTitle.push_back("video1");
+    ytChannel.publishedVideoTitle.push_back("video2");
 
-    cout << "Name" << " " << ytChannel.Name << endl;
-    cout << "OwnerName" << " " << ytChannel.OwnerName << endl;
-    cout << "Subscriber Count" << " " << ytChannel.subscriberCount << endl;
-    cout << "Published Video Title" << endl;
-    for(string videoTitle:ytChannel.publishedVideoTitle) {
-          cout << videoTitle << endl;   
-    }
+   ytChannel.printInformation();
 
 
 
